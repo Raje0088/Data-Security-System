17c921124b3758d5f3054760fc429832ace16381
Raje0088 <rajchincholkar58@gmail.com>
2026-09-05 18:59:47 +0530

    added config port

--- frontend/src/socketio/socket.js
+++ frontend/src/socketio/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client'
+import { base_url } from '../config/config';
 
-const host = window.location.hostname;
-const socket = io(`http://${host}:3000`, {
+const socket = io(base_url, {
     transports: ["websocket"],
     withCredentials: true,
     autoConnect: true,
@@ -10,5 +10,16 @@ const socket = io(`http://${host}:3000`, {
     reconnectionDelay: 1000,             // start at 1s
     reconnectionDelayMax: 10000,
 }) // backend port
+
+// const host = window.location.hostname;
+// const socket = io(`http://${host}:3000`, {
+//     transports: ["websocket"],
+//     withCredentials: true,
+//     autoConnect: true,
+//     reconnection: true,
+//     reconnectionAttempts: Infinity,      // ♾ keep trying
+//     reconnectionDelay: 1000,             // start at 1s
+//     reconnectionDelayMax: 10000,
+// }) // backend port
 
 export default socket;  
